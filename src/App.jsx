5418f26f0cabb920f4d185f31c307953391320e4
Ace <aceonsteam001@gmail.com>
2023-03-12 21:20:44 +0800

    add: starter files for cha

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 import Header from './components/Header'
+import SectionBox from './components/section-box'
 
 function App() {
   return (
     <div className="container">
       <Header />
+
+      <section>
+        <p>EmoWall Confessions</p>
+
+        <SectionBox />
+      </section>
+
     </div>
   )
 }
---
+++ src/components/section-box/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './section-box.css'
+
+function SectionBox() {
+  return (
+    <div className='ace'>
+      ICON Love
+
+    </div>
+  )
+}
+
+export default SectionBox
