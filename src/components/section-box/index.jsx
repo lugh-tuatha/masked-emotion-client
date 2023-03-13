277471a5d697770c6cda71c508e60a83f3cb3395
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-13 11:21:31 +0800

    edit responsiveness

--- src/components/section-box/index.jsx
+++ src/components/section-box/index.jsx
@@ -3,13 +3,10 @@ import './section-box.css'
 
 function SectionBox() {
   return (
-    <div className='emo-sec'>
       <div className="emo-box">
         <h4>ICON</h4>
         <p>Love</p>
       </div>
- 
-    </div>
   )
 }
 
