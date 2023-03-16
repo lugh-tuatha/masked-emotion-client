548476858587c46bb68a7b7abf213f425aa602d7
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-16 17:14:35 +0800

    added post top

--- src/components/create-post/index.jsx
+++ src/components/create-post/index.jsx
@@ -3,7 +3,15 @@ import './create-post.css'
 
 function CreatePost() {
   return (
-    <div>CreatePost</div>
+    <div>
+      <div className="Content">
+        <h4>EVER lOVED SOMEONE YOU COULDN'T TELL?</h4>
+          <div class="btn">
+            <button>lOVE</button>
+            <button>Create A Post</button>
+          </div>
+      </div>
+    </div>
   )
 }
 
