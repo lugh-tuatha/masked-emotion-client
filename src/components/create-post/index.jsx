0e579c24c8e11225e972abb7a157ee674d7ee92b
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-16 17:31:18 +0800

    added post

--- src/components/create-post/index.jsx
+++ src/components/create-post/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './create-post.css'
+import * as Ai from "react-icons/ai";
 
 function CreatePost() {
   return (
     <div>
       <div className="Content">
-        <h4>EVER lOVED SOMEONE YOU COULDN'T TELL?</h4>
-          <div class="btn">
-            <button>lOVE</button>
-            <button>Create A Post</button>
+        <h6>EVER lOVED SOMEONE YOU COULDN'T TELL?</h6>
+          <div className='button1'>
+            <div className='btn'><Ai.AiFillHeart /> LOVE</div>
+            <div className='btn'><Ai.AiOutlinePlus /> CREATE POST</div>
           </div>
       </div>
     </div>
