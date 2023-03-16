973b7381ff6d5005f2191d84734f8683cd6908bb
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-16 18:06:00 +0800

    edit heart

--- src/components/create-post/index.jsx
+++ src/components/create-post/index.jsx
@@ -9,7 +9,7 @@ function CreatePost() {
         <h6>EVER lOVED SOMEONE YOU COULDN'T TELL?</h6>
           <div className='button1'>
               <div className="dropdown">
-                <div className="btn"><Ai.AiOutlineHeart /> LOVE</div>
+                <div className="btn"><Ai.AiFillHeart /> LOVE</div>
                 <div className="dropdown-content">
                   <a href="#">Sadness</a>
                   <a href="#">Anger</a>
