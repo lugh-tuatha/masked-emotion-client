afb7771ff7d19a8f754429fe3b645369fc4fc351
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-16 17:52:35 +0800

    added dropdown

--- src/components/create-post/index.jsx
+++ src/components/create-post/index.jsx
@@ -8,7 +8,14 @@ function CreatePost() {
       <div className="Content">
         <h6>EVER lOVED SOMEONE YOU COULDN'T TELL?</h6>
           <div className='button1'>
-            <div className='btn'><Ai.AiFillHeart /> LOVE</div>
+              <div className="dropdown">
+                <div className="btn"><Ai.AiOutlineHeart /> LOVE</div>
+                <div className="dropdown-content">
+                  <a href="#">Link 1</a>
+                  <a href="#">Link 2</a>
+                  <a href="#">Link 3</a>
+                </div>
+              </div>
             <div className='btn'><Ai.AiOutlinePlus /> CREATE POST</div>
           </div>
       </div>
