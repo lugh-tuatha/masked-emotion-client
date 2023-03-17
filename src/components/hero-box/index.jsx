1ba272fb00fc734b4df72aec23bca2f485cc363a
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-17 10:57:06 +0800

    button

--- src/components/hero-box/index.jsx
+++ src/components/hero-box/index.jsx
@@ -8,30 +8,35 @@ import Arrow from '../../assets/icons/arrow.svg'
 
 function heroBox() {
   return (
-    <div className='col-lg-6 hero-gal'>
-      <div className="hero-box">
-        <h3>10K</h3>
-        <p>Over 10,000 Active Users</p>
+    <div className='col-lg-6'>
+      <div className="hero-gal">
+        <div className="hero-box">
+          <h3>10K</h3>
+          <p>Over 10,000 Active Users</p>
+        </div>
+        <div className="hero-box">
+          <img src={Computer} alt="" />
+          <p>Easy-to-use interface</p>
+        </div>
+        <div className="hero-box">
+          <img src={Fruits} alt="" />
+          <p>Discover different perspective</p>
+        </div>
+        <div className="hero-box">
+          <img src={Chat} alt="" />
+          <p>Engage with community</p>
+        </div>
+        <div className="hero-box">
+          <img src={Box} alt="" />
+          <p>Be part of something bigger</p>
+        </div>
+        <div className="hero-box">
+          <img src={Arrow} alt="" />
+          <p>Stay anonymous</p>
+        </div>
       </div>
-      <div className="hero-box">
-        <img src={Computer} alt="" />
-        <p>Easy-to-use interface</p>
-      </div>
-      <div className="hero-box">
-        <img src={Fruits} alt="" />
-        <p>Discover different perspective</p>
-      </div>
-      <div className="hero-box">
-        <img src={Chat} alt="" />
-        <p>Engage with community</p>
-      </div>
-      <div className="hero-box">
-        <img src={Box} alt="" />
-        <p>Be part of something bigger</p>
-      </div>
-      <div className="hero-box">
-        <img src={Arrow} alt="" />
-        <p>Stay anonymous</p>
+      <div className="cpost">
+        <button id='btn3'>Create a post</button>
       </div>
     </div>
   )
