b430635cb0bd627fd01fa633b5ce98274736847a
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-17 10:41:24 +0800

    finish boxes

--- src/components/hero-box/index.jsx
+++ src/components/hero-box/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './hero-box.css'
 import Computer from '../../assets/icons/comp.svg'
+import Fruits from '../../assets/icons/fruits.svg'
+import Chat from '../../assets/icons/chat.svg'
+import Box from '../../assets/icons/box.svg'
+import Arrow from '../../assets/icons/arrow.svg'
 
 function heroBox() {
   return (
@@ -14,19 +18,19 @@ function heroBox() {
         <p>Easy-to-use interface</p>
       </div>
       <div className="hero-box">
-        <img src={Computer} alt="" />
+        <img src={Fruits} alt="" />
         <p>Discover different perspective</p>
       </div>
       <div className="hero-box">
-        <img src={Computer} alt="" />
+        <img src={Chat} alt="" />
         <p>Engage with community</p>
       </div>
       <div className="hero-box">
-        <img src={Computer} alt="" />
+        <img src={Box} alt="" />
         <p>Be part of something bigger</p>
       </div>
       <div className="hero-box">
-        <img src={Computer} alt="" />
+        <img src={Arrow} alt="" />
         <p>Stay anonymous</p>
       </div>
     </div>
