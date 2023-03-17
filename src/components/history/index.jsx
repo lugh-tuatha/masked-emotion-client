47ad02ad2e190fba246569e6a6f5fc8e53fb2bc7
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-17 11:41:44 +0800

    edit history

--- src/components/history/index.jsx
+++ src/components/history/index.jsx
@@ -23,19 +23,19 @@ function History() {
         <div className="hstryCntnt">
           <div className='hstryIcon'>
             <Fa.FaRegDotCircle />
-            <p id='nmber'><b>02</b></p><span><b>Our Mission</b></span>
+            <p id='nmber'><b>02</b></p><span><b>Our History</b></span>
           </div> 
             <div className="Cntnt">
-              <p>At EmoWall, our mission is to provide a safe and supportive online community for individuals to express their emotions, connect with others who share similar experiences, and find resources to help them navigate the challenges of life.</p>
+              <p>EmoWall was founded in 2023 with a vision to create a safe and inclusive online space where individuals can freely express their thoughts and emotions without fear of judgment or criticism.</p>
             </div>
         </div>
         <div className="hstryCntnt">
           <div className='hstryIcon'>
             <Fa.FaRegDotCircle />
-            <p id='nmber'><b>03</b></p><span><b>Our Mission</b></span>
+            <p id='nmber'><b>03</b></p><span><b>Our Team</b></span>
           </div> 
             <div className="Cntnt">
-              <p>At EmoWall, our mission is to provide a safe and supportive online community for individuals to express their emotions, connect with others who share similar experiences, and find resources to help them navigate the challenges of life.</p>
+              <p>Our team is made up of passionate individuals who are dedicated to providing a safe and supportive space for users to express their emotions. We pride ourselves on our expertise in website moderation and user support and our commitment to providing the best possible experience for our users.</p>
             </div>
         </div>
       </div>
