1733166ae189c567d4095717113d73d73251ced0
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-17 10:29:04 +0800

    added margin

--- src/components/hero-box/index.jsx
+++ src/components/hero-box/index.jsx
@@ -4,14 +4,30 @@ import Computer from '../../assets/icons/comp.svg'
 
 function heroBox() {
   return (
-    <div className='herp-gal'>
+    <div className='col-lg-6 hero-gal'>
       <div className="hero-box">
         <h3>10K</h3>
         <p>Over 10,000 Active Users</p>
       </div>
       <div className="hero-box">
         <img src={Computer} alt="" />
-        <p>Over 10,000 Active Users</p>
+        <p>Easy-to-use interface</p>
+      </div>
+      <div className="hero-box">
+        <img src={Computer} alt="" />
+        <p>Discover different perspective</p>
+      </div>
+      <div className="hero-box">
+        <img src={Computer} alt="" />
+        <p>Engage with community</p>
+      </div>
+      <div className="hero-box">
+        <img src={Computer} alt="" />
+        <p>Be part of something bigger</p>
+      </div>
+      <div className="hero-box">
+        <img src={Computer} alt="" />
+        <p>Stay anonymous</p>
       </div>
     </div>
   )
