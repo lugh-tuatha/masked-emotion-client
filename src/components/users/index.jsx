066d2c14ed597f844b46bb3e8244bd4ddb0f5e4b
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-19 00:41:52 +0800

    added page

--- src/components/users/index.jsx
+++ src/components/users/index.jsx
@@ -25,6 +25,17 @@ function Users() {
                     <td><a href="#"><Ai.AiFillDelete/></a></td>
                 </tr>
             </table>
+
+            <div className="pagination">
+                <a href="#">&laquo;</a>
+                <a href="#" class="active">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
+                <a href="#">6</a>
+                <a href="#">&raquo;</a>
+            </div>
         </div>
     )
 }
