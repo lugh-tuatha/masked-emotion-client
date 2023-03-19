5dabece11d4ba17347f57a567ac5d3f6c7d4b1ad
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-19 15:32:34 +0800

    edit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,16 +3,12 @@ import UserDetails from '../components/Header'
 
 import Header from '../components/Header'
 import Users from '../components/users'
-import Love from '../assets/section-icon/love.svg'
-import Sad from '../assets/section-icon/sadness.svg'
 
 function About() {
   return (
     <div className='container'>
       <Header />
       < Users />
-      <img src={Love} alt="" />
-      <img src={Sad} alt="" />
     </div>
   )
 }
