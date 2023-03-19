e24f8d430c5bccd6b9b3c18be679626cadee1305
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-19 13:56:30 +0800

    rename

--- src/components/admin-sidebar/index.jsx
+++ src/components/admin-sidebar/index.jsx
@@ -4,8 +4,8 @@ import './admin-sidebar.css'
 function Admin_Sidebar() {
     return (
         <div className='admin-sidebar'>
-            <a href="#">Admin</a>
             <a href="#">User</a>
+            <a href="#">Post</a>
         </div>
     )
 }
