0e343f291ceaf2ba9a90784382a7d28058189fe4
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-19 21:50:08 +0800

    edit active

--- src/components/user-list/index.jsx
+++ src/components/user-list/index.jsx
@@ -76,8 +76,8 @@ function UserList({ userData }) {
 
       <div class="pagination">
         <a href="#">&laquo;</a>
-        <a href="#">1</a>
-        <a href="#" class="active">2</a>
+        <a href="#" class="active">1</a>
+        <a href="#" >2</a>
         <a href="#">3</a>
         <a href="#">4</a>
         <a href="#">5</a>
