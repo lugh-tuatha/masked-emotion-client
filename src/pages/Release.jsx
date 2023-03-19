7b3b5559f4e0756e9a12f6a5c4521ac6e25c7162
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-19 23:00:53 +0800

    added background

--- src/pages/Release.jsx
+++ src/pages/Release.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './pages.css'
 
 import sectionBoxData from '../json/sectionBoxes.json'
 
@@ -16,8 +17,13 @@ function Release() {
   ));
 
   return (
-    <div>
-      <Header />
+    <div className='all-body'>
+            <Header />
+      <div class="custom-shape-divider-top-1679237202">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+        </svg>
+    </div>
       <main className="main-content container">
         <section className="home-section px-5">
           <p>EmoWall Confessions</p>
@@ -33,7 +39,6 @@ function Release() {
           </div>
         </section>
       </main>
-
       <UserList />
     </div>
   )
