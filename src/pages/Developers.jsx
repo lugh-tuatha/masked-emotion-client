c4d7117d61238cb7e0cacb01fae9fd93e987b085
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-20 00:49:45 +0800

    edit dev

--- src/pages/Developers.jsx
+++ src/pages/Developers.jsx
@@ -3,12 +3,15 @@ import './pages.css'
 
 import Header from '../components/Header';
 import Devs from '../components/devs'
+import Footer from '../components/footer'
 
 function Developers() {
   return (
     <div>
       <Header/>
+      <h1 className='text-center mt-5'>The Developers</h1>      
       <Devs />
+      <Footer />
     </div>
   )
 }
