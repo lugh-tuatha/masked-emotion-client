a06364de8892cb5441c2823aab6d127d826cfdf5
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-19 13:53:48 +0800

    added sidebar

---
+++ src/components/admin-sidebar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './admin-sidebar.css'
+
+function Admin_Sidebar() {
+    return (
+        <div className='admin-sidebar'>
+            <a href="#">Admin</a>
+            <a href="#">User</a>
+        </div>
+    )
+}
+
+export default Admin_Sidebar
