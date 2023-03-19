cd1ffc5cdc4e045d9fe8bfc0e7200913bb2bca39
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-03-19 23:23:32 +0800

    change bg

--- src/components/devs/index.jsx
+++ src/components/devs/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './devs.css'
 
+
 import * as Bs from "react-icons/bs";
 
 function Devs() {
--- src/pages/Developers.jsx
+++ src/pages/Developers.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './pages.css'
 
+import Header from '../components/Header';
 import Devs from '../components/devs'
 
 function Developers() {
   return (
     <div>
+      <Header/>
       <Devs />
     </div>
   )
