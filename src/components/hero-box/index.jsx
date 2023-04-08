f3c02ccb81b5f22cbd6df50b7b077e47bd23a268
Ace <aceonsteam001@gmail.com>
2023-04-08 16:26:56 +0800

    fix: tab spacing

--- src/components/hero-box/index.jsx
+++ src/components/hero-box/index.jsx
@@ -12,30 +12,30 @@ function heroBox() {
   return (
     <div>
       <div className="hero-gal">
-          <div className="hero-box">
-            <h3 className='counter'>10K</h3>
-            <p >Over 10,000 Active Users</p>
-          </div>
-          <div className="hero-box">
-            <img src={Computer} alt="" />
-            <p>Easy-to-use interface</p>
-          </div>
-          <div className="hero-box">
-            <img src={Fruits} alt="" />
-            <p>Discover different perspective</p>
-          </div>
-          <div className="hero-box">
-            <img src={Chat} alt="" />
-            <p>Engage with community</p>
-          </div>
-          <div className="hero-box">
-            <img src={Box} alt="" />
-            <p>Be part of something bigger</p>
-          </div>
-          <div className="hero-box">
-            <img src={Arrow} alt="" />
-            <p>Stay anonymous</p>
-          </div>
+        <div className="hero-box">
+          <h3 className='counter'>10K</h3>
+          <p >Over 10,000 Active Users</p>
+        </div>
+        <div className="hero-box">
+          <img src={Computer} alt="" />
+          <p>Easy-to-use interface</p>
+        </div>
+        <div className="hero-box">
+          <img src={Fruits} alt="" />
+          <p>Discover different perspective</p>
+        </div>
+        <div className="hero-box">
+          <img src={Chat} alt="" />
+          <p>Engage with community</p>
+        </div>
+        <div className="hero-box">
+          <img src={Box} alt="" />
+          <p>Be part of something bigger</p>
+        </div>
+        <div className="hero-box">
+          <img src={Arrow} alt="" />
+          <p>Stay anonymous</p>
+        </div>
       </div>
       <div className="cpost">
         <Link to="/release"><Button >Create a post</Button></Link>
