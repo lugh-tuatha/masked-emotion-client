51c6395b9b56f8886f35c2fee8d9b7b55a3598c1
Ace <aceonsteam001@gmail.com>
2023-07-22 15:11:32 +0800

    improve: navbar mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,19 +2,33 @@ import React from 'react'
 import './header.css';
 import { NavLink, Link} from "react-router-dom";
 import { FaBars } from 'react-icons/fa';
+
+import { HiOutlineBars3 } from 'react-icons/hi2'
 import logo from '../../assets/icons/logo.png';
 
 function index() {
   return (
     <div className='container-width'>
-      <nav className=' home-nav'>
-        <Link to="/"><img src={logo} /></Link>
+      <nav className='home-nav'>
+        <div className='flex justify-between items-center w-full'>
+          <img src={logo} alt="" />
 
-        <ul>
-          <NavLink to="/"><li>Home</li></NavLink>
-          <NavLink to="/release"><li>Release</li></NavLink>
-          <NavLink to="/faqs"><li>FAQs</li></NavLink>
-        </ul>
+          <ul className='md:flex gap-8 font-bold hidden '>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/release'>Release</Link></li>
+            <li><Link to='/faqs'>Faqs</Link></li>
+          </ul>
+
+          <div className='md:hidden'>
+            <HiOutlineBars3 size={36}/>
+
+            <ul className='w-full bg-white absolute left-0 top-0 h-screen'>
+              <li className='font-2xl'><Link to='/'>Home</Link></li>
+              <li className='font-2xl'><Link to='/release'>Release</Link></li>
+              <li className='font-2xl'><Link to='/faqs'>Faqs</Link></li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   )
