68391217d159f209aec9cac5794d0ce633cb1e5d
Ace <114063817+lugh-tuatha@users.noreply.github.com>
2023-08-17 22:23:33 +0800

    add: send your question alert

--- src/pages/Faqs.jsx
+++ src/pages/Faqs.jsx
@@ -24,17 +24,29 @@ function Faqs() {
 
         <div className='mt-4'>
           {faqsData.map((faqs, index) => (
-            <div key={index} className={`duration-200 mb-2 ${faqs.isOpened ? 'bg-red-200' : 'bg-gray-200'}`}>
+            <div key={index} className={`duration-200 mb-2 ${faqs.isOpened ? 'bg-[#FF6882] text-black' : 'bg-[#081F4B] text-white'}`}>
               <div className='px-4 py-3 cursor-pointer flex items-center justify-between gap-2'
                 onClick={() => toggleCollapse(index)}>
                 <p>{faqs.question}</p>
                 {faqs.isOpened ? <Ai.AiOutlineMinus size={24} /> : <Ai.AiOutlinePlus size={24} />}
               </div>
               <Collapse isOpened={faqs.isOpened}>
-                <div className='px-4 py-2'>{faqs.answer}</div>
+                <div className='px-4 pb-2 text-sm'>{faqs.answer}</div>
               </Collapse>
             </div>
           ))}
+        </div>
+
+        <h1 className="text-3xl font-bold mt-4">Do you have a question you'd like to ask us?</h1>
+
+        <div className="mt-4 bg-[#FF6882] rounded-lg px-20 py-5">
+          <div className='flex justify-center gap-2'>
+            <p>Our dm's is open kindly send your question at </p>
+            <span className='inline-flex items-center gap-1'>
+              <Ai.AiOutlineMail size={20}/> 
+              <p className='inline-block font-bold'>acegabriel0710@gmail.com</p>
+            </span>
+          </div>
         </div>
       </div>
     </MainLayout>
